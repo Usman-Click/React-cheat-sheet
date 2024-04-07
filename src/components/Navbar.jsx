850735a0615a84ac3b03136dfbf2c79d699908a8
usman-click <uadamuharuna@gmail.com>
2024-04-07 10:16:48 +0100

    Active navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import logo from '../assets/img/logo.png'
+import { NavLink } from 'react-router-dom'
 
 
 function Navbar() {
-  return (
-    
+
+  const activeBtn = ({isActive}) => isActive ? 
+  'bg-white text-indigo-600 rounded-md py-2 px-4 hover:opacity-50' :
+   'text-white hover:opacity-50'
+
+  return (    
     <>
     <nav className="bg-indigo-700 p-5 border-b-indigo-600 border-b">
         <div className=" container mx-auto flex justify-between">
 
+            <div className="flex items-center">
             <img src={logo} alt="" className="w-10 h-10 rounded-full hover:opacity-50 " />
+            <h1 className="font-bold text-white text-2xl ml-2">React Jobs</h1>
+
+            </div>
 
             <div className='flex justify-between items-center space-x-4 '>
-                <a href="#" className="bg-white text-indigo-600 rounded-md py-2 px-4 hover:opacity-50">
+                <NavLink to="/" className={activeBtn}>
                   Home
-                </a>
-                <a href="#" className=' text-white hover:opacity-50'>
+                </NavLink>
+                <NavLink to="/jobs" className={activeBtn}>
                   Jobs
-                </a>
-                <a href="#" className=' text-white hover:opacity-50'>
+                </NavLink>
+                <NavLink to="add-jobs" className={activeBtn}>
                   Add Job
-                </a>
+                </NavLink>
             </div>
 
         </div>
